02cb65cb9ecce3bcbbff2fb31beb6dee52fe8e69
Al Mondragón <al.drago@live.com>
2021-05-07 18:43:18 -0500

    Add assets

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,18 @@ export default function Home() {
     <div className='text-[#202046] text-[32px]'>
       <Head>
         <title>Bootcamp Testimonials Slider</title>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel='icon' href='/favicon.png' />
       </Head>
 
-      <main>
-        <h2>“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”</h2>
+      <main className='min-h-screen bg-black relative'>
+        <div className='flex flex-wrap'>
+          <h2 className='font-light'>
+            “ I’ve been interested in coding for a while but never taken the
+            jump, until now. I couldn’t recommend this course enough. I’m now in
+            the job of my dreams and so excited about the future. ”
+          </h2>
+          <img src='/image-tanya.jpg' alt='Tanya' />
+        </div>
       </main>
     </div>
   )
