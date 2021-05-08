5838144cb772745eb780dd51b2fb8af04be92d6b
Al Mondragón <al.drago@live.com>
2021-05-07 22:06:10 -0500

    Fix components directory location

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import SliderController from './components/SliderController'
+import SliderController from '../components/SliderController'
 
 export default function Home() {
   return (
